0dd91dc0d802304c1abdff29ce3dbde361cbca28
Zero123-star <55027354+Zero123-star@users.noreply.github.com>
2025-01-07 20:20:47 +0000

    Update inregistrat.php
    modificat, adaugat max

--- htdocs/inregistrat.php
+++ htdocs/inregistrat.php
@@ -3,7 +3,7 @@ session_start();
 $pas = $_POST["pas1"];
 $nume = $_POST["nume"];
 $email = $_POST["email"];
-require_once 'db.php';
+require_once '../db.php';
 if ($conn->error)
     echo "Eroare conectare data de baze. Revino mai tarziu";
 $aux = $conn->prepare("Select * from utilizator where nume=?");
@@ -11,7 +11,7 @@ $aux->bind_param('s',$nume);
 $aux->execute();
 $aux=$aux->get_result();
 $delta = $aux->fetch_row();
-$aux2 = $conn->query("Select count(*) from utilizator");
+$aux2 = $conn->query("Select max(ifnull(id,2)) from utilizator");
 $Beta = $aux2->fetch_column();
 $Beta+=1;
 if($delta==null)
@@ -19,6 +19,8 @@ if($delta==null)
 $aux=$conn->prepare("Insert into utilizator(email,id,nume,parola,rol) values (?,?,?,?,'Utilizator')");
 //echo "Insert into utilizator(email,id,nume,parola,rol) values ($email,$Beta,$nume,$pas,'Utilizator')";
 $aux->bind_param('siss',$email,$Beta,$nume,$pas);
+
+echo "Insert into utilizator(email,id,nume,parola,rol) values ($email,$Beta,$nume,$pas,'Utilizator')";
 $aux->execute();
 echo "Utilizatorul a fost inregistrat cu succes!";
 }
